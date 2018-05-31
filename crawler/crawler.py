8dc84b530f30f10b289b11db848600172b62569f
Michael Krausse <michael.krausse@directbox.com>
2018-05-31 22:19:59 +0200

    extrahiert die 69 bis 70 keys

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,9 +2,13 @@ import urllib
 import json
 import time
 import sys
+import codecs
 
 adrkey=6900000
 sleeper=1
+
+#ausgabe = open("69_70.txt","w", "utf-8")
+ausgabe = codecs.open("69_70.txt", "w", "utf-8")
 
 while adrkey<7000000:
 
@@ -15,8 +19,8 @@ while adrkey<7000000:
     time.sleep(sleeper)
     try:
         data_row = json.loads(myfile)["d"]
-        print data_row["Pstlz"]+"::::"+data_row["Street"]+"::::"+data_row["Houseno"]+ "::::"+data_row["Service_day_regular"]+"::::"+ data_row["Service_date"]+"::::"+data_row["Adrkey"]+"::::"+data_row["Rhythm"]
-        sys.stdout.flush()
+        ausgabe.write(data_row["Pstlz"]+"::::"+data_row["Street"]+"::::"+data_row["Houseno"]+ "::::"+data_row["Service_day_regular"]+"::::"+ data_row["Service_date"]+"::::"+data_row["Adrkey"]+"::::"+data_row["Rhythm"]+"\n")
     except ValueError:
         pass
     adrkey = adrkey + 1
+ausgabe.close()
