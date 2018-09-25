94f9721c63a5c06ffe7fb5dd1dda1e81d56f46da
Michael Krausse michael.krausse@directbox.com <michael.krausse@directbox.com>
2018-09-25 12:50:55 +0200

    eslint configured

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -3,17 +3,19 @@ import logo from './logo.svg';
 import Search from './Search';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Der Gelbe Sack Reporter</h1>
-                </header>
-                <Search />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        {' '}
+        <header className="App-header">
+          {' '}
+          <img src={logo} className="App-logo" alt="logo" />{' '}
+          <h1 className="App-title">Der Gelbe Sack Reporter</h1>{' '}
+        </header>{' '}
+        <Search />{' '}
+      </div>
+    );
+  }
 }
 
 export default App;
