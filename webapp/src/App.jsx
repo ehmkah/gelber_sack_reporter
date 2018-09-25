32b3e9b4b3cd1442f15bc11dcca625d0e9c831ff
Michael Krausse michael.krausse@directbox.com <michael.krausse@directbox.com>
2018-09-25 12:14:48 +0200

    two simple files

---
+++ webapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Search from './Search';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Der Gelbe Sack Reporter</h1>
+                </header>
+                <Search />
+            </div>
+        );
+    }
+}
+
+export default App;
