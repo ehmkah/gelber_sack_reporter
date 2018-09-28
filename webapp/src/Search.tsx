8084c9a42bfbac7f1f55879f72073070ae576c22
Michael Krausse michael.krausse@directbox.com <michael.krausse@directbox.com>
2018-09-28 13:19:58 +0200

    example for enzyme test with typescript (:-o)

--- webapp/src/Search.tsx
+++ webapp/src/Search.tsx
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
-class Search extends Component {
+interface State {
+  zipCode: string;
+}
+
+class Search extends Component<object, State> {
+  public state = {
+    zipCode: "1234"
+  };
+
+  public foo() {
+    const newVal: State = { zipCode: "4561" };
+    this.setState(newVal);
+  }
+
   public render() {
     return <div>hallo</div>;
   }
 }
 
 export default Search;
+export { State };
--- webapp/src/__tests__/Search.test.tsx
+++ webapp/src/__tests__/Search.test.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { create } from "react-test-renderer";
-import Search from "../Search";
+import Enzyme, { shallow } from "enzyme";
+import Search, { State } from "../Search";
+import Adapter from "enzyme-adapter-react-16";
 
 test("snapshot", () => {
   const c = create(<Search />);
   expect(c.toJSON()).toMatchSnapshot();
+});
+
+test("example unit test", () => {
+  Enzyme.configure({ adapter: new Adapter() });
+  const wrapper = shallow(<Search />);
+  const state = wrapper.state() as State;
+  expect(state.zipCode).toBe("1234");
+  const seacrh = wrapper.instance() as Search;
+  seacrh.foo();
+  const state2 = wrapper.state() as State;
+  expect(state2.zipCode).toBe("4561");
 });
